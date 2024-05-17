0f374d0d5a3ce2731101f41a7d95e68d07de7b4b
zhiwumao <zhiwumao@outlook.com>
2024-05-17 23:10:19 +0800

    完善

--- src/main.ts
+++ src/main.ts
@@ -9,10 +9,16 @@ const app = createApp(App).use(router).use(createPinia())
 
 app.config.globalProperties.$http = http
 
+// 注册所有图标
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+
 // 全局组件注册
-import components from '@/components/index'
-Object.keys(components).forEach((key) => {
-  app.component(key, components[key])
-})
+// import components from '@/components/index'
+// Object.keys(components).forEach((key) => {
+//   app.component(key, components[key])
+// })
 
 app.mount('#app')
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,24 +1,18 @@
-import { createRouter, createWebHistory } from "vue-router";
-import Test1 from '../components/Test1/index.vue'
-import Test2 from '../components/Test2/index.vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import Login from '@/views/login/index.vue'
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        {
-            path: '/login',
-            component: Login
-        },
-        {
-            path: '/test1',
-            component: Test1
-        },
-        {
-            path: '/test2',
-            component: Test2
-        }
-    ]
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      component: Login
+    },
+    {
+      path: '/login',
+      component: Login
+    }
+  ]
 })
 
 export default router
