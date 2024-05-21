d1ea1a3f11392f39882a32c8b23c917ca6eaece4
zhiwumao <zhiwumao@outlook.com>
2024-05-21 22:20:44 +0800

    完善ts

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,10 +1,10 @@
 import request from '@/utils/request'
 import type { IReqLogin, IResLogin } from '@/types/user'
 
-export function loginApi(data: IReqLogin): Promise<IResLogin> {
+export function loginApi(param: IReqLogin): Promise<IResLogin> {
   return request({
     url: '/web/api/auth/login',
     method: 'post',
-    data
+    data: param
   })
 }
